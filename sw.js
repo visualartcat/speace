01d12675f980fb04ddd8471a132d041e9fe726ac
visualartcat <xxsujeong@gmail.com>
2026-09-19 00:35:44 +0900

    주문 제조 순서 레시피와 선택 피드백 개선

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'euphoria-manual-v5';
+const CACHE = 'euphoria-manual-v6';
 const CORE = ['/', '/index.html', '/manifest.webmanifest', '/assets/icon-192.png', '/assets/icon-512.png'];
 
 self.addEventListener('install', event => {
