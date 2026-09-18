7d3e40741a38e0304ed3b02c6ed1ad02c8b56ecc
visualartcat <xxsujeong@gmail.com>
2026-09-18 23:47:45 +0900

    카페 레시피와 테이블별 주문 작업판 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'euphoria-manual-v4';
+const CACHE = 'euphoria-manual-v5';
 const CORE = ['/', '/index.html', '/manifest.webmanifest', '/assets/icon-192.png', '/assets/icon-512.png'];
 
 self.addEventListener('install', event => {
